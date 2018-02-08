c93f077b014addf97388ffc53948b8bbb830a088
Emory Merryman <emory.merryman@gmail.com>
2018-02-08 15:47:16 +0000

    use named volume not env var

--- root/bin/docker.sh
+++ root/bin/docker.sh
@@ -10,7 +10,7 @@ sudo \
     --rm \
     --label expiry=$(($(date +%s)+60*60*24*7)) \
     --volume /var/run/docker.sock:/var/run/docker.sock:ro \
-    --volume ${EXTERNAL_DOCKER_VOLUME}:/srv/docker \
+    --volume docker:/srv/docker \
     --workdir $(pwd) \
     docker:17.12.0 \
         "${@}"
