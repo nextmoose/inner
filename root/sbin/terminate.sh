3cdefdfeb3ef6cad18ee8fecbae6044fa5919ce4
Emory Merryman <emory.merryman@gmail.com>
2018-02-11 12:24:11 +0000

    change command

--- root/sbin/terminate.sh
+++ root/sbin/terminate.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ps -e -o uid=,pid=,command= | grep "^ *1000 *" | sed "s#^ *1000 *##" | grep " *node /opt/docker/c9sdk/server.js --listen 0.0.0.0 -w /opt/docker/workspace -p ${CLOUD9_PORT}\$" | sed -e "s# *node /opt/docker/c9sdk/server.js --listen 0.0.0.0 -w /opt/docker/workspace -p ${CLOUD9_PORT}\$##"
+ps -e -o uid=,pid=,command= | grep "^ *1000 *" | sed "s#^ *1000 *##" | grep " *node /opt/docker/c9sdk/server.js --listen 0.0.0.0 -w /opt/docker/workspace -p ${CLOUD9_PORT}\$" | sed -e "s# *node /opt/docker/c9sdk/server.js --listen 0.0.0.0 -w /opt/docker/workspace -p ${CLOUD9_PORT}\$##" | while read PID
 do
     kill ${PID}
 done
