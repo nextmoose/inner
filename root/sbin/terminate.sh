451c7390ea09c3b1428ecd6d35abde3c75c0f55f
Emory Merryman <emory.merryman@gmail.com>
2018-02-11 22:35:08 +0000

    preparation commit

--- root/sbin/terminate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-ps -e -o uid=,pid=,command= | grep "^ *1000 *" | sed "s#^ *1000 *##" | grep " *node /opt/docker/c9sdk/server.js --listen 0.0.0.0 -w /opt/docker/workspace -p ${CLOUD9_PORT}\$" | sed -e "s# *node /opt/docker/c9sdk/server.js --listen 0.0.0.0 -w /opt/docker/workspace -p ${CLOUD9_PORT}\$##" | while read PID
-do
-    kill ${PID}
-done &&
-    kill -1
