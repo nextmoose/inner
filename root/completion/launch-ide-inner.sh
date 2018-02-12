063908b55a25fbf96815dc08e09c6838850e160c
Emory Merryman <emory.merryman@gmail.com>
2018-02-12 02:50:01 +0000

    fix it

--- root/completion/launch-ide-inner.sh
+++ root/completion/launch-ide-inner.sh
@@ -5,7 +5,7 @@ _UseGetOpt() {
         COMPREPLY=() &&
         CUR=${COMP_WORDS[COMP_CWORD]} &&
         case "${CUR}" in
-            -)
+            -*)
                 COMPREPLY=($( compgen -W "--host-name --host-port --master-branch --upstream-id-rsa --upstream-organization --upstream-repository --origin-id-rsa --origin-organization --origin-repository  --report-id-rsa --report-organization --report-repository --project-name --cloud9-port --checkout-branch --gpg-secret-key --gpg2-secret-key --gpg-owner-trust --gpg2-owner-trust --gpg-key-id --user-name --user-email --user-" -- ${CUR}))
             ;;
         esac
