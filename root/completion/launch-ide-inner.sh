78ac0bde8e8c49daca2ec968862bf37f302713f7
Emory Merryman <emory.merryman@gmail.com>
2018-02-12 02:51:11 +0000

    fix it

--- root/completion/launch-ide-inner.sh
+++ root/completion/launch-ide-inner.sh
@@ -6,7 +6,7 @@ _UseGetOpt() {
         CUR=${COMP_WORDS[COMP_CWORD]} &&
         case "${CUR}" in
             -*)
-                COMPREPLY=($( compgen -W "--host-name --host-port --master-branch --upstream-id-rsa --upstream-organization --upstream-repository --origin-id-rsa --origin-organization --origin-repository  --report-id-rsa --report-organization --report-repository --project-name --cloud9-port --checkout-branch --gpg-secret-key --gpg2-secret-key --gpg-owner-trust --gpg2-owner-trust --gpg-key-id --user-name --user-email --user-" -- ${CUR}))
+                COMPREPLY=($( compgen -W "--host-name --host-port --master-branch --upstream-id-rsa --upstream-organization --upstream-repository --origin-id-rsa --origin-organization --origin-repository  --report-id-rsa --report-organization --report-repository --project-name --cloud9-port --checkout-branch --gpg-secret-key --gpg2-secret-key --gpg-owner-trust --gpg2-owner-trust --gpg-key-id --user-name --user-email" -- ${CUR}))
             ;;
         esac
   return 0
