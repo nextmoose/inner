9daf9c306db1771d01b459698e6c3087efce0e17
Emory Merryman <emory.merryman@gmail.com>
2018-03-02 21:03:48 +0000

    adding

---
+++ extension/bin/artifacts-get-multiple.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+while [ ${#} -gt 0 ]
+do
+    case ${1} in
+        --name)
+            export NAME="${2}" &&
+                shift 2
+        ;;
+        --timestamp)
+            export TSTAMP="${2}" &&
+                shift 2
+        ;;
+        *)
+            echo Unknown Option &&
+                echo ${0} &&
+                echo ${@} &&
+                exit 64
+        ;;
+    esac
+done &&
+    source /usr/local/bin/artifacts-env &&
+    (
+        pass show aws-access-key-id &&
+            pass show aws-secret-access-key &&
+            echo us-east-1 &&
+            echo text
+    ) | aws configure &&
+    mkdir parts &&
+    PARTS_COUNT=$(aws s3api get-object --bucket ${BUCKET} --key ${NAME}-${TSTAMP}.tar.gz --part 1 parts/00000001 --query "PartsCount") &&
+    seq 2 ${PARTS_COUNT} | while read PART
+    do
+        aws s3api get-object --bucket ${BUCKET} --key ${NAME}-${TSTAMP}.tar.gz --part ${PART}
+    done &&
+    
