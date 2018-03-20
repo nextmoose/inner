e8bcf782fa015bfffd6b88120b01cd3f81dee902
Emory Merryman <emory.merryman@gmail.com>
2018-03-20 04:26:34 +0000

    shutdown cleanup commit

--- extension/bin/create-codecommit-repository.sh
+++ extension/bin/create-codecommit-repository.sh
@@ -3,6 +3,10 @@
 while [ ${#} -gt 0 ]
 do
     case ${1} in
+        --repository-name)
+            REPOSITORY_NAME="${2}" &&
+                shift 2
+        ;;
         *)
             echo Unknown Option &&
                 echo ${0} &&
@@ -11,4 +15,4 @@ do
         ;;
     esac
 done &&
-    
+    aws codecommit create-repository --repository-name ${REPOSITORY_NAME}
