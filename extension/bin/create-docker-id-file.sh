70bd49a9b3e60d2e5ce1860ea0fb926a258ad17d
Emory Merryman <emory.merryman@gmail.com>
2018-04-09 11:32:53 +0000

    fix create-docker-id-file

--- extension/bin/create-docker-id-file.sh
+++ extension/bin/create-docker-id-file.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkdir --parents /opt/cloud9/workspace/docker &&
-    ID_FILE=$(mktemp /opt/cloud9/workspace/XXXXXXXX) &&
+mkdir --parents ${WORKSPACE_DIR}/docker &&
+    ID_FILE=$(mktemp ${WORKSPACE_DIR}/XXXXXXXX) &&
     rm --force ${ID_FILE} &&
     echo ${ID_FILE}
