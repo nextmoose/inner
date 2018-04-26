050e921f19af1622e30eb3037073dcb12b774f5b
Emory Merryman <emory.merryman@gmail.com>
2018-04-26 11:24:49 +0000

    wtf

--- extension/bin/launch-gitlab-runner.sh
+++ extension/bin/launch-gitlab-runner.sh
@@ -7,6 +7,10 @@ do
             CONFIGURATION_FILE="${2}" &&
                 shift 2
         ;;
+        --configuration-volume)
+            CONFIGURATION_VOLUME="${2}" &&
+                shift 2
+        ;;
         *)
             echo Unsupported Option &&
                 echo ${0} &&
@@ -19,6 +23,13 @@ done &&
     then
         source "${CONFIGURATION_FILE}"
     fi
-    docker container create --name gitlab-runner --restart-always gitlab/gitlab-runner:v10.7.0 &&
+    docker \
+        container \
+        create \
+        --name gitlab-runner \
+        --restart-always \
+        --mount type=volume,source=${CONFIGURATION_VOLUME},destination=/etc/gitlab-runner,readonly=false \
+        --env DOCKER_HOST \
+        gitlab/gitlab-runner:v10.7.0 &&
     docker network connect main gitlab-runner &&
     docker container start gitlab-runner
