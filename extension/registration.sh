2ad8b23b86683c0af08c07763e4dcd52e6972e1c
Emory Merryman <emory.merryman@gmail.com>
2018-04-26 12:07:25 +0000

    fuck it

---
+++ extension/registration.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+gitlab-runner \
+    register \
+    --url http://gitlab/ \
+    --non-interactive \
+    --registration-token jPwoEgyFZzbduZP3JJzM \
+    --name root \
+    --executor docker \
+    --docker-image docker:18.02.0-ce \
+    --docker-host ${DOCKER_HOST}
