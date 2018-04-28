d908f05b9345eaa43b5f0af69a8f860e7e03b4f2
Emory Merryman <emory.merryman@gmail.com>
2018-04-28 22:01:03 +0000

    done

--- extension/registration.sh
+++ extension/registration.sh
@@ -5,9 +5,11 @@ gitlab-runner \
     --url http://gitlab/ \
     --non-interactive \
     --registration-token jPwoEgyFZzbduZP3JJzM \
-    --name root \
+    --name special \
     --executor docker \
     --docker-image docker:18.02.0-ce \
     --docker-network-mode main \
+    --docker-network-mode special \
+    --tag-list "alpha" \
     --env DOCKER_HOST="${DOCKER_HOST}" \
     --docker-host ${DOCKER_HOST}
