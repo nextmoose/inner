aa08fe348e103c1621d03f72ec12a569a428ca1a
Emory Merryman <emory.merryman@gmail.com>
2018-06-01 17:29:36 +0000

    retarded copy

--- extension/bin/restore-home.sh
+++ extension/bin/restore-home.sh
@@ -11,6 +11,6 @@ sudo dnf install --assumeyes rsync dvdisaster genisoimage &&
     seq 0 6 | while read I
     do
         aws s3 cp s3://hp-pavillion/home.tar.gz.0${I}.gpg.iso hp-pavillion/home.tar.gz.0${I}.gpg.iso &&
-            docker container run -it --rm --volume $(pwd):/in:ro --volume /srv/host/home/user:/out alpine:3.4 cp /in/home.tar.gz.0${I}.gpg.iso &&
+            docker container run -it --rm --volume $(pwd):/in:ro --volume /srv/host/home/user:/out alpine:3.4 cp /in/home.tar.gz.0${I}.gpg.iso /out/home.tar.gz.0${I}.gpg.iso &&
             rm home.tar.gz.0${I}.gpg.iso
     done
