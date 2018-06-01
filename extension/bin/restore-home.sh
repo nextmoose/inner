52573b5447c76dbbc31dac57cefcfb979c950812
Emory Merryman <emory.merryman@gmail.com>
2018-06-01 21:36:07 +0000

    fuck

--- extension/bin/restore-home.sh
+++ extension/bin/restore-home.sh
@@ -12,6 +12,6 @@ sudo dnf install --assumeyes rsync dvdisaster genisoimage &&
     for I in $(seq 0 6)
     do
         aws s3 cp s3://hp-pavillion/home.tar.gz.0${I}.gpg.iso hp-pavillion/home.tar.gz.0${I}.gpg.iso &&
-            docker container run --interactive --rm --volume $(pwd):hp-pavillion/in:ro --volume /srv/host/home/user:/out alpine:3.4 cp /in/hp-pavillion/home.tar.gz.0${I}.gpg.iso /out/home.tar.gz.0${I}.gpg.iso &&
+            docker container run --interactive --rm --volume $(pwd):/in:ro --volume /srv/host/home/user:/out alpine:3.4 cp /in/hp-pavillion/home.tar.gz.0${I}.gpg.iso /out/home.tar.gz.0${I}.gpg.iso &&
             rm home.tar.gz.0${I}.gpg.iso
     done
