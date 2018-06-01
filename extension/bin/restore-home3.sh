d7bd0adea340dcf899b3e7016c1be10cb4af4434
Emory Merryman <emory.merryman@gmail.com>
2018-06-01 23:44:56 +0000

    add 3rd script

---
+++ extension/bin/restore-home3.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd $(mktemp -d /opt/cloud9/workspace/XXXXXXXX) &&
+    docker container run --interactive --volume /srv/host/home/user:/in:ro --volume $(pwd):/out cp in/home.tar.gz.gpg out/home.tar.gz.gpg &&
+    gpg home.tar.gz.gpg &&
+    gunzip home.tar.gz &&
+    tar --extract --file home.tar &&
+    pwd
