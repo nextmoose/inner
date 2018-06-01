8ccf7006c8bc8c8e8e995ec54e3ea8d78e47ee8b
Emory Merryman <emory.merryman@gmail.com>
2018-06-01 17:08:48 +0000

    fucking semicolon

--- extension/bin/restore-home.sh
+++ extension/bin/restore-home.sh
@@ -7,10 +7,10 @@ sudo dnf install --assumeyes rsync dvdisaster genisoimage &&
             echo us-east-1 &&
             echo text
     ) | aws configure &&
-    cd $(mktemp -d /opt/cloud9/workspace//XXXXXXXX) &&
+    cd $(mktemp -d /opt/docker/workspace/XXXXXXXX) &&
     seq 0 6 | while read I
     do
         aws s3 cp s3://hp-pavillion/home.tar.gz.0${I}.gpg.iso hp-pavillion/home.tar.gz.0${I}.gpg.iso &&
-            docker container run -it --rm --volume $(pwd);/in:ro --volume /srv/host/home/user:/out alpine:3.4 cp /in/home.tar.gz.0${I}.gpg.iso &&
+            docker container run -it --rm --volume $(pwd):/in:ro --volume /srv/host/home/user:/out alpine:3.4 cp /in/home.tar.gz.0${I}.gpg.iso &&
             rm home.tar.gz.0${I}.gpg.iso
     done
