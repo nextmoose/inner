f18f50cdea64ebade3b5104d0533fe6eeb265959
Emory Merryman <emory.merryman@gmail.com>
2018-06-02 00:48:11 +0000

    wtf

--- extension/bin/restore-home3.sh
+++ extension/bin/restore-home3.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 cd $(mktemp -d /opt/cloud9/workspace/XXXXXXXX) &&
-    docker container run --interactive --volume /srv/host/home/user:/in:ro --volume $(pwd):/out alpine:3.4 cp in/home.tar.gz.gpg out/home.tar.gz.gpg &&
-    gpg home.tar.gz.gpg &&
+    for I in $(seq 0 6)
+    do
+        docker container run --interactive --volume /srv/host/home/user:/in:ro --volume $(pwd):/out alpine:3.4 cp in/home.tar.gz.0${I}.gpg out/home.tar.gz.0${I}.gpg &&
+            gpg home.tar.gz.0${I}.gpg
+            cat home.tar.gz.0${I} >> home.tar.gz
+    done &&
     gunzip home.tar.gz &&
     tar --extract --file home.tar &&
     pwd
