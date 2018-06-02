da35c90eb0124293d52dbb6e7f60fe0c455d8254
Emory Merryman <emory.merryman@gmail.com>
2018-06-02 00:54:23 +0000

    use memorized passphrase

--- extension/bin/restore-home3.sh
+++ extension/bin/restore-home3.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-cd $(mktemp -d /opt/cloud9/workspace/XXXXXXXX) &&
+PASSPHRASE=${1} &&
+    cd $(mktemp -d /opt/cloud9/workspace/XXXXXXXX) &&
     for I in $(seq 0 6)
     do
         docker container run --interactive --volume /srv/host/home/user:/in:ro --volume $(pwd):/out alpine:3.4 cp in/home.tar.gz.0${I}.gpg out/home.tar.gz.0${I}.gpg &&
-            gpg home.tar.gz.0${I}.gpg
+            echo ${PASSPHRASE} | gpg home.tar.gz.0${I}.gpg --passphrase-fd 0 &&
             cat home.tar.gz.0${I} >> home.tar.gz
     done &&
     gunzip home.tar.gz &&
