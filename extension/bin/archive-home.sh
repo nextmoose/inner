4ed5d24b888a5b23e62215efe55cc195d5dfb6bd
Emory Merryman <emory.merryman@gmail.com>
2018-06-02 00:00:07 +0000

    it failed try again

--- extension/bin/archive-home.sh
+++ extension/bin/archive-home.sh
@@ -8,7 +8,7 @@ export PASSPHRASE=${1} &&
             echo us-east-1 &&
             echo text
     ) | aws configure &&
-#    cd /tmp/tmp.mtlVfFP63Y &&
+    cd $(mktemp -d) &&
     (cat <<EOF
 rm -rf /tmp/volumes &&
     mkdir -p /tmp/volumes &&
@@ -37,8 +37,16 @@ EOF
             mkisofs -o ${FILE}.gpg.iso -r -J splits/${FILE}.gpg &&
             rm splits/${FILE}.gpg &&
             dvdisaster --image ${FILE}.gpg.iso -mRS01 --redundancy high --create ${FILE}.gpg.iso --ecc ${FILE}.gpg.iso.ecc &&
-            aws s3 cp ${FILE}.gpg.iso s3://hp-pavillion/${FILE}.gpg.iso &&
-            aws s3 cp ${FILE}.gpg.iso s3://hp-pavillion/${FILE}.gpg.iso.ecc &&
+            while ! aws s3 cp ${FILE}.gpg.iso s3://hp-pavillion/${FILE}.gpg.iso
+            do
+                echo FAILED TO UPLOAD &&
+                    sleep 60
+            done &&
+            while ! aws s3 cp ${FILE}.gpg.iso s3://hp-pavillion/${FILE}.gpg.iso.ecc
+            do
+                echo FAILED TO UPLOAD &&
+                    sleep 60
+            done &&
             rm ${FILE}.gpg.iso ${FILE}.gpg.iso.ecc
     done
 
