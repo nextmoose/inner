851a4a9967513aca44e41689c0437ff96be0e72c
Emory Merryman <emory.merryman@gmail.com>
2018-06-02 00:44:00 +0000

    wtf

--- extension/bin/restore-home2.sh
+++ extension/bin/restore-home2.sh
@@ -2,10 +2,9 @@
 
 for I in $(seq 0 6)
 do
-    rm -rf mount &&
-        mkdir -p mount &&
-        sudo mount -o loop home.tar.gz.0${I}.gpg.iso mount &&
-        cp mount/home.tar.gz.0${I}.gpg home.tar.gz.0${I}.gpg &&
+    mkdir -p mount.0${I} &&
+        sudo mount -o loop home.tar.gz.0${I}.gpg.iso mount.0${I} &&
+        cp mount.0${I}/home.tar.gz.0${I}.gpg home.tar.gz.0${I}.gpg &&
         sleep 10 &&
-        sudo umount mount
+        sudo umount mount.0${I}
 done
