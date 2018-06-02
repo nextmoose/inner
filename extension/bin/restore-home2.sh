082ad2e0ff695297df8e7a9bdf18ece6de09a9fd
Emory Merryman <emory.merryman@gmail.com>
2018-06-02 00:05:26 +0000

    i am

--- extension/bin/restore-home2.sh
+++ extension/bin/restore-home2.sh
@@ -5,7 +5,7 @@ do
     rm -rf mount &&
         mkdir -p mount &&
         sudo mount -o loop home.tar.gz.0${I}.gpg.iso mount &&
-        cp mount/home/tar.gz.0${I}.gpg home.tar.gz.0${I}.gpg &&
+        cp mount/home.tar.gz.0${I}.gpg home.tar.gz.0${I}.gpg &&
         gpg home.tar.gz.0${I}.gpg &&
         sleep 10 &&
         sudo umount mount
