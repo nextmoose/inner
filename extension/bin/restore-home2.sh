5faa570ea6c1f39343531fd1f90d20de0ed5731e
Emory Merryman <emory.merryman@gmail.com>
2018-06-02 00:04:08 +0000

    i am

--- extension/bin/restore-home2.sh
+++ extension/bin/restore-home2.sh
@@ -7,7 +7,6 @@ do
         sudo mount -o loop home.tar.gz.0${I}.gpg.iso mount &&
         cp mount/home/tar.gz.0${I}.gpg home.tar.gz.0${I}.gpg &&
         gpg home.tar.gz.0${I}.gpg &&
-        cat home.tar.gz.0${I} >> home.tar.gz &&
         sleep 10 &&
         sudo umount mount
 done
