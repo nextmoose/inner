f4630787ca6d7f0b4e617ae25231943d07053b49
Emory Merryman <emory.merryman@gmail.com>
2018-06-03 10:41:57 +0000

    wtf

--- scripts/up.sh
+++ scripts/up.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-docker network create main &&
+docker     volume     create     --label "volume.expiration=never"     --label "volume.retention=always"     --label "snapshot.expiry=1 week"     --label "snapshot.retention=1 day"     --label "archive.shallow.expiry=1 week"     --label "archive.shallow.retention=1 month"     --label "archive.deep.expiry=1 month"     --label "archive.deep.retention=1 year"     docker         container         create         --name browser         --mount 
+    docker network create main &&
     docker network create gitlab &&
-    docker \
-        container \
-        create \
-        --
+    docker network connect main browser &&
+    docker container start browser
