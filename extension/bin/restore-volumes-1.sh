6dc9b3f935d39f85c13479ff6b16b5679636126d
Emory Merryman <emory.merryman@gmail.com>
2018-06-04 11:15:09 +0000

    the aws s3 ... provides the leading zeroes

--- extension/bin/restore-volumes-1.sh
+++ extension/bin/restore-volumes-1.sh
@@ -11,7 +11,7 @@ sudo dnf install --assumeyes rsync dvdisaster genisoimage &&
     cd $(mktemp -d /opt/cloud9/workspace/XXXXXXXX) &&
     for I in $(aws s3 ls hp-pavillion | grep volumes.tar.gz | grep "iso\$" | sed -e "s#^.*volumes.tar[.]gz[.]##" | sed -e "s#[.]gpg[.]iso\$##")
     do
-        aws s3 cp s3://hp-pavillion/volumes.tar.gz.0${I}.gpg.iso hp-pavillion/volumes.tar.gz.0${I}.gpg.iso &&
-            docker container run --interactive --rm --volume $(pwd):/in:ro --volume /srv/host/volumes/user:/out alpine:3.4 cp /in/hp-pavillion/volumes.tar.gz.0${I}.gpg.iso /out/volumes.tar.gz.0${I}.gpg.iso &&
-            rm hp-pavillion/volumes.tar.gz.0${I}.gpg.iso
+        aws s3 cp s3://hp-pavillion/volumes.tar.gz.${I}.gpg.iso hp-pavillion/volumes.tar.gz.${I}.gpg.iso &&
+            docker container run --interactive --rm --volume $(pwd):/in:ro --volume /srv/host/volumes/user:/out alpine:3.4 cp /in/hp-pavillion/volumes.tar.gz.${I}.gpg.iso /out/volumes.tar.gz.${I}.gpg.iso &&
+            rm hp-pavillion/volumes.tar.gz.${I}.gpg.iso
     done
